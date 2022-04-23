31c3eb0e25b35152d4cd2cbf3436d857503c0c93
vpaklatzis <vpaklatzis@gmail.com>
2022-04-23 22:49:47 +0300

    ..

---
+++ recommendation-service/src/main/java/com/microservices/recommendationservice/RecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.recommendationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationServiceApplication.class, args);
+	}
+
+}
